85d29849c76ca73de936211192b4a9028d05bf48
Romeq03 <romekbartek18@gmail.com>
2024-01-14 15:27:04 +0100

    first commit

---
+++ foreach/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] pets = { "cat", "dog", "parrot", "guinea pig" };
+
+        Console.WriteLine("Wywołanie pętli foreach na elementach pets:");
+
+        foreach (string pet in pets)
+        {
+
+            if (pet == "dog")
+            {
+                
+                Console.WriteLine("End");
+                continue; // Przerwij pętlę, gdy natrafisz na "dog"
+            }
+            Console.WriteLine(pet);
+        }
+
+        Console.ReadLine(); // Zatrzymuje konsolę przed zamknięciem
+    }
+}
